34713211406ed0bf69f0a5a3b50baf5df451654b
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-10-21 16:17:36 -0400

    working on the layout

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,14 +5,14 @@ import "./styles.css";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
-            <a className="nav-link" href="/"><h2 className="roll">My Next Roll</h2></a>
+            <a className="nav-link" href="/home"><h2 className="roll">My Next Roll</h2></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/profile">Profile</a>
