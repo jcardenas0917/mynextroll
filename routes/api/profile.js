9a40e447bd44a13bb2dd4d105648b240c6596c3d
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-10-26 13:44:26 -0400

    continue working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,6 @@
 const { ensureAuthenticated } = require("../../config/auth");
 const router = require("express").Router();
 const userController = require("../../controller/userController");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
 
 // Matches with "/api/profile"
 router.route("/", ensureAuthenticated)
--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 const express = require("express");
-const flash = require("connect-flash");
-const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const passport = require("passport");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Passport Config
-require("./config/passport")(passport);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Express session
-app.use(session({ secret: "superfan", resave: true, saveUninitialized: true }));
-
-// Passport Middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect Flash
-app.use(flash());
-
-// Global Vars
-app.use((req, res, next) => {
-    res.locals.successMsg = req.flash("successMsg");
-    res.locals.errorMsg = req.flash("errorMsg");
-    res.locals.error = req.flash("error");
-    next();
-});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
