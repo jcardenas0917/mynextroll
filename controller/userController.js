65eccc96c0f3823325ae52f9c53bbf31346ca9bb
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-10-30 15:30:33 -0400

    Deleted unused files

--- controller/userController.js
+++ controller/userController.js
@@ -8,6 +8,12 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
+    findOne: function (req, res) {
+        db.Profile
+            .findOne({ email: req.params.email })
+            .then(dbModel = res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
     findById: function (req, res) {
         db.Profile
             .findById(req.params.id)
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./profile");
 
-// Book routes
+
 router.use("/profile", userRoutes);
 
 module.exports = router;
